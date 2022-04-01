a5a70946891c4e023267b566eb704fb570ea4b13
Sachin <17sachind@gmail.com>
2022-04-02 01:21:03 +0530

    added gradient files and gradients to cards

--- app/src/main/java/com/my/cogitateapp/LoginActivity.java
+++ app/src/main/java/com/my/cogitateapp/LoginActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.my.cogitateapp.R;
+
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 
