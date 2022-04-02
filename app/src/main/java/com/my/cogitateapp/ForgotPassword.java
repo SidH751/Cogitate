626c847d791c25c62b22272bbde6af9dd759e561
Sachin <17sachind@gmail.com>
2022-04-02 14:35:38 +0530

    forgot password activities, xml modified

--- app/src/main/java/com/my/cogitateapp/ForgotPassword.java
+++ app/src/main/java/com/my/cogitateapp/ForgotPassword.java
@@ -1,18 +1,13 @@
 package com.my.cogitateapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPassword extends AppCompatActivity {
@@ -31,52 +26,29 @@ public class ForgotPassword extends AppCompatActivity {
         mAuth= FirebaseAuth.getInstance();
 
         emailForgot = findViewById(R.id.forgotEmail);
-        submitForgot=findViewById(R.id.submitForgotButton);
+        submitForgot = findViewById(R.id.submitForgotButton);
         backLogin=findViewById(R.id.backToLogin);
-        backLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(ForgotPassword.this,LoginActivity.class));
-            }
-        });
+        backLogin.setOnClickListener(
+                view -> startActivity(new Intent(ForgotPassword.this,LoginActivity.class)));
 
-        submitForgot.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                validateData();
-            }
-        });
+        submitForgot.setOnClickListener(view -> validateData());
     }
-    void validateData()
-    {
-        email= emailForgot.getText().toString();
-        if(email.isEmpty())
-        {
+    void validateData() {
+        email = emailForgot.getText().toString();
+        if(email.isEmpty()) {
             emailForgot.setError("Field cannot be empty");
-        }
-        else
-        {
+        } else {
             passwordForgot();
         }
     }
-    void passwordForgot()
-    {
-        mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if(task.isSuccessful())
-                {
-                    Toast.makeText(ForgotPassword.this,"Check your email to change password",Toast.LENGTH_LONG).show();
-                    startActivity(new Intent(ForgotPassword.this,LoginActivity.class));
-                }
-                else
-                {
-                    Toast.makeText(ForgotPassword.this,"Error: "+task.getException().getMessage(),Toast.LENGTH_LONG).show();
-                }
+    void passwordForgot() {
+        mAuth.sendPasswordResetEmail(email).addOnCompleteListener(task -> {
+            if(task.isSuccessful()) {
+                Toast.makeText(ForgotPassword.this,"Check your email to change password",Toast.LENGTH_LONG).show();
+                startActivity(new Intent(ForgotPassword.this,LoginActivity.class));
+            } else {
+                Toast.makeText(ForgotPassword.this,"Error: "+task.getException().getMessage(),Toast.LENGTH_LONG).show();
             }
         });
     }
-
-
-
 }
