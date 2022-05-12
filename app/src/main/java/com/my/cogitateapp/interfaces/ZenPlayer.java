79b33c1aa5073afcc852cfea47f8704775f19220
SidH <siddhanth751@gmail.com>
2022-05-12 18:57:38 +0530

    Added music player ui only

--- app/src/main/java/com/my/cogitateapp/interfaces/ZenPlayer.java
+++ app/src/main/java/com/my/cogitateapp/interfaces/ZenPlayer.java
@@ -2,15 +2,135 @@ package com.my.cogitateapp.interfaces;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.my.cogitateapp.R;
 
+import java.io.File;
+import java.util.ArrayList;
+
+
 public class ZenPlayer extends AppCompatActivity {
+    Button btnPlay, btnNext, btnPrevious, btnFastForward, btnFastBackWard;
+    TextView txtSongName, txtSongStart, txtSongEnd;
+    SeekBar seekMusicBar;
+    ImageView imageView;
+
+
+
+
+
+    String songName;
+    public static final String EXTRA_NAME = "song_name";
+    static MediaPlayer mediaPlayer;
+    int position;
+
+    ArrayList<File> mySongs;
+
+    Thread updateSeekBar;
+    private Object view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zen_player);
+        btnPlay = (Button) findViewById(R.id.BtnPlay);
+        btnNext = (Button) findViewById(R.id.BtnNext);
+        btnPrevious = (Button) findViewById(R.id.BtnPrevious);
+        btnFastForward = (Button) findViewById(R.id.BtnFastForward);
+        btnFastBackWard = (Button) findViewById(R.id.BtnFastRewind);
+        
+
+        txtSongName = (TextView) findViewById(R.id.SongTxt);
+        txtSongStart = (TextView) findViewById(R.id.TxtSongStart);
+        txtSongEnd = (TextView) findViewById(R.id.TxtSongEnd);
+        imageView = (ImageView) findViewById(R.id.MusicImage);
+
+        if (mediaPlayer != null) {
+
+            //we will start mediaPlayer if currently there is no songs in it
+            mediaPlayer.start();
+            mediaPlayer.release();
+        }
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Checking playing any songs or not
+                if (mediaPlayer.isPlaying()) {
+
+                    //setting the play icon
+                    btnPlay.setBackgroundResource(R.drawable.play_song_icon);
+
+                    //Pausing the current media
+                    mediaPlayer.pause();
+
+                } else {
+
+                    //Setting the pause icon
+                    btnPlay.setBackgroundResource(R.drawable.pause_song_icon);
+
+                    //Starting the media player
+                    mediaPlayer.start();
+
+                    //Creating the Animation
+                    TranslateAnimation moveAnim = new TranslateAnimation(-25, 25, -25, 25);
+                    moveAnim.setInterpolator(new AccelerateInterpolator());
+                    moveAnim.setDuration(600);
+                    moveAnim.setFillEnabled(true);
+                    moveAnim.setFillAfter(true);
+                    moveAnim.setRepeatMode(Animation.REVERSE);
+                    moveAnim.setRepeatCount(1);
+
+                    //Setting the Animation for the Image
+                    imageView.startAnimation(moveAnim);
+
+                    //Calling the BarVisualizer
+
+                }
+            }
+        });
+
+
+
     }
+    public void startAnimation(View view, Float degree) {
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(imageView, "rotation", 0f, degree);
+        objectAnimator.setDuration(1000);
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(objectAnimator);
+        animatorSet.start();
+
+    }
+
+    public String createDuration(int duration) {
+
+        String time = "";
+        int min = duration / 1000 / 60;
+        int sec = duration / 1000 % 60;
+
+        time = time + min + ":";
+
+        if (sec < 10) {
+
+            time += "0";
+
+        }
+        time += sec;
+        return time;
+
+    }
+
 }
